33e74c29b35a571a33dbc6e91c1fdabc14691ac4
nzomkxia <z82507200@gmail.com>
2018-06-14 17:39:59 +0800

    add copyright header

--- src/main/java/com/alibaba/dubboadmin/web/pulltool/DateFormatUtil.java
+++ src/main/java/com/alibaba/dubboadmin/web/pulltool/DateFormatUtil.java
@@ -1,5 +1,4 @@
 
-package com.alibaba.dubboadmin.web.pulltool;
 /*
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
@@ -16,6 +15,8 @@ package com.alibaba.dubboadmin.web.pulltool;
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+package com.alibaba.dubboadmin.web.pulltool;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
